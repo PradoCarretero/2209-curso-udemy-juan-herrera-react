e63d681a1bdcbb50a069647228aff875ae83ccce
PradoCarretero <p.carreteroayala@gmail.com>
2022-09-22 11:43:16 +0200

    add ex lesson 2

---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,12 @@
+const nombre = "Prado";
+const apellido = "Ca";
+
+/* const nombreCompleto = nombre + " " + apellido; */
+const nombreCompleto = `${nombre} ${apellido}`;
+console.log(nombreCompleto);
+
+function getSaludo(nombre) {
+  return "Hola " + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}`);
